e06c184f7d68b4c2cc39a31672bad6b633211f67
Gustavo S. Fischer <gustavosfischer@gmail.com>
2025-10-27 10:20:39 -0300

    feat/esqueleto

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import './style.css'
+
+export default function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2025 - Algum dia vai ter direitos reservados - Gustavo S. Fischer</p>
+        </footer>
+    )
+}
---
+++ src/pages/JavaScript/index.jsx
@@ -0,0 +1,12 @@
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+import './style.css'
+
+export default function JavaScript() {
+    return (
+        <>
+            <Header titulo={'JavaScript'}/>
+            <Footer/>
+        </>
+    )
+}
